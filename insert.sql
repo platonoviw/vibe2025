28d19fe582296b519f3d25e9aa6093a3dba142e1
platonoviw <hjyujhgfjh@gmail.com>
2025-06-16 17:51:32 +0300

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO todolist.accounts (`username`, `passhash`) VALUES ('root', '4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2');
+INSERT INTO todolist.accounts (`username`, `passhash`) VALUES ('user', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
+INSERT INTO todolist.accounts (`username`, `passhash`) VALUES ('test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08');
+
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (1, 'etrytjuyi,ukymjnty');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (2, '"£$%^&"');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (3, 'hm,ukhjggnjh');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (1, ' vb vcbvn');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (3, 'r4ty5uyjuk,ymtnr');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (1, '4t5465j6m6um');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (2, 'ny5nm67,9,8');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (2, 's21s1323d24f44234g55h5j57j');
